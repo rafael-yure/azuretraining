c6f3ea317202899e20bcc51c64521157fbe3ad90
Rafael Braga <rafaelysbraga@gmail.com>
2020-02-03 11:49:20 -0300

    Adjustments for tomcat deployment

--- src/main/java/com/mastercard/azuretraining/AzureTrainingApplication.java
+++ src/main/java/com/mastercard/azuretraining/AzureTrainingApplication.java
@@ -2,12 +2,19 @@ package com.mastercard.azuretraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AzureTrainingApplication {
+public class AzureTrainingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureTrainingApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AzureTrainingApplication.class);
 	}
 
 }
