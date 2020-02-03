cc50842adae5e0faaecca0a44111252bf2070e7f
Rafael Braga <rafael.braga@mastercard.com>
2020-02-03 06:57:54 -0300

    Initial project setup

---
+++ src/test/java/com/mastercard/azuretraining/AzureTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercard.azuretraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
